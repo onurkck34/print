c7e24127e066924baf2682d9bf1896fac8a2e286
Onur Koçak <98240992+onurkck34@users.noreply.github.com>
2022-03-31 20:01:55 +0300

    Update print.py

--- print.py
+++ print.py
@@ -1,15 +1,5 @@
 print("hello world")
 print("onur")
-a = 5
-b = 4
-c = 10
-print(a*b**c)
-
-if a == 5:
-  print("True!")
-elif b == 4:
-  print("True!")
-elif c == 10:
-  print("True!")
-else:
-  print("False")
+print("""Hello World""")
+print('Hello World')
+print('''Hello World''')
